8b65adc3ed8089cf03fe2bc6a9d26754f038a02e
Romen Rodriguez-Gil <rrodriguezgil@turo.com>
2022-11-30 11:16:30 +0000

    feat: integrate extended preset into default
    We have tested in with turo/web-authentication and there are no disruptive changes.
    And there is value including some react-specific plugins and rules as default.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 module.exports = {
   root: true,
+  plugins: ["react", "react-hooks", "jsx-a11y"],
   env: {
     browser: true,
   },
   ignorePatterns: ["babel.config.js"],
-  extends: ["@open-turo/eslint-config-typescript/legacy"],
+  extends: [
+    "@open-turo/eslint-config-typescript/legacy",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        specialLink: ["to"],
+      },
+    ],
     "node/no-unpublished-import": [
       "error",
       {
@@ -18,5 +35,25 @@ module.exports = {
         ],
       },
     ],
+    // don't force .jsx extension
+    "react/jsx-filename-extension": "off",
+    // In TS you must use the Fragment syntax instead of the shorthand
+    "react/jsx-fragments": "off",
+    // This allows props to be spread in JSX
+    "react/jsx-props-no-spreading": "off",
+    // ensure props are alphabetical
+    "react/jsx-sort-props": "error",
+    // We don't need default props on TS components
+    "react/require-default-props": "off",
+    "react/sort-prop-types": "error",
+    // State initialization can be in the constructor or via class fields
+    "react/state-in-constructor": "off",
+    // This allows static properties to be placed within the class declaration
+    "react/static-property-placement": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
