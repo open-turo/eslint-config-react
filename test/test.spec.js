b03422b4d9cfb3afc35ed8e099d615c2871d491e
Victor <victor@turo.com>
2025-02-24 16:35:23 +0000

    feat: add flat configs and use eslint v9 by default
    BREAKING CHANGE: The default exports from this module are now eslint
    flat configurations.

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,30 +1,67 @@
-const fs = require("fs");
-const { ESLint } = require("eslint");
+import { loadESLint } from "eslint";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 describe("validate config", () => {
-  it(`load config file in ESLint to validate all rules are correct`, async () => {
-    const stringConfig = fs.readFileSync("./test/sample.ts", "utf8");
+  const testFileName = "component.jsx";
+  const code = fs.readFileSync(
+    path.resolve(__dirname, "__fixtures__", testFileName),
+    "utf8",
+  );
 
-    const cli = new ESLint({
-      overrideConfigFile: "./index.js",
+  test("the legacy recommended config is correct", async () => {
+    // Unlike the new flat config, the legacy turo config only works with TS, so we create a simple tsconfig.json file
+    // in the __fixtures__ directory to make it work
+    const ESLint = await loadESLint({
+      useFlatConfig: false,
     });
-
+    const linter = new ESLint({
+      overrideConfig: {
+        parserOptions: {
+          tsconfigRootDir: path.resolve(__dirname, "__fixtures__"),
+        },
+      },
+      overrideConfigFile: "./recommended.cjs",
+    });
+    const messages = await linter.lintFiles(
+      path.resolve(__dirname, "__fixtures__", testFileName),
+      {
+        cwd: path.resolve(__dirname, "__fixtures__"),
+      },
+    );
+    expect(messages[0].messages).toEqual([]);
+    expect(messages[0].errorCount).toEqual(0);
     const calculatedConfig =
-      await cli.calculateConfigForFile("./test/sample.ts");
-
-    // remove parser property from calculated config because it is env specific (fails in CI)
-    const { parser, ...calculatedConfigWithoutParserProperty } =
-      calculatedConfig;
-    // print all the rules
-    expect(calculatedConfigWithoutParserProperty).toMatchSnapshot();
-
-    const lintResult = await cli.lintText(stringConfig);
-
-    /**
-     * We just need to validate a value is returned.
-     * If there is an issue with the config, `cli.lintText` will error,
-     * and we will never hit this line so the test will fail.
-     */
-    expect(lintResult).toBeTruthy();
+      await linter.calculateConfigForFile("./test/sample.ts");
+    expect(calculatedConfig.rules).toMatchSnapshot();
   });
+
+  test.each(["index.mjs", "index.cjs"])(
+    `the flat config is correct for %s`,
+    async (configFile) => {
+      const ESLint = await loadESLint({
+        useFlatConfig: true,
+      });
+      const { default: config } = await import(`../${configFile}`);
+      const linter = new ESLint({
+        baseConfig: config,
+        overrideConfig: [
+          {
+            files: [testFileName],
+          },
+        ],
+      });
+      const messages = await linter.lintText(code, {
+        filePath: testFileName,
+      });
+      expect(messages[0].messages).toEqual([]);
+      expect(messages[0].errorCount).toEqual(0);
+      const calculatedConfig =
+        await linter.calculateConfigForFile("./test/sample.ts");
+      expect(calculatedConfig.rules).toMatchSnapshot();
+    },
+  );
 });
