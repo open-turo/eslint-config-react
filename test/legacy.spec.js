a8fa7461e7521c16171c6ce8a226ef48a842d90e
Brant Barger <bbarger@turo.com>
2025-03-05 19:43:16 +0000

    test: add snapshot tests of lint rule output

---
+++ test/legacy.spec.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const { ESLint } = require("eslint");
+
+describe("validate config", () => {
+  it(`Legacy - load config file in ESLint to validate all rules are correct`, async () => {
+    const stringConfig = fs.readFileSync("./test/sample.ts", "utf8");
+
+    const cli = new ESLint({
+      overrideConfigFile: "./legacy.js",
+    });
+
+    const calculatedConfig =
+      await cli.calculateConfigForFile("./test/sample.ts");
+
+    // remove parser property from calculated config because it is env specific (fails in CI)
+    const { parser, ...calculatedConfigWithoutParserProperty } =
+      calculatedConfig;
+    // print all the rules
+    expect(calculatedConfigWithoutParserProperty).toMatchSnapshot();
+
+    const lintResult = await cli.lintText(stringConfig);
+
+    /**
+     * We just need to validate a value is returned.
+     * If there is an issue with the config, `cli.lintText` will error,
+     * and we will never hit this line so the test will fail.
+     */
+    expect(lintResult).toBeTruthy();
+  });
+});
